35158ebd7cd8e857f64aa09ee8780dadcd1d2002
Kepins <sz.kepinski1@gmail.com>
2023-01-16 22:38:32 +0100

    Automated spliting in 8 second of audio.

--- noise.py
+++ noise.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-audiopath = "/home/pawel/clonning/audiofiles/out/splits"
+audiopath = "audiofiles/splits"
 dataset_name = "test-dataset"
 import os
 import subprocess
@@ -7,27 +7,31 @@ import soundfile as sf
 import pyloudnorm as pyln
 import sys
 import glob
-rnn = "/home/pawel/clonning/repos/rnnoise/examples/rnnoise_demo"
+import tempfile
+rnn = "repos/rnnoise/examples/rnnoise_demo"
  
 paths = glob.glob(os.path.join(audiopath, '*.wav'))
  
+tempdir = tempfile.TemporaryDirectory()
+print(tempdir.name)
+
 for filepath in paths:
  
   base = os.path.basename(filepath)
-  tp_s = "" + dataset_name + "/" + "converted" + "/"
-  tf_s = "" + dataset_name + "/" + "converted" + "/" + base
+  tp_s = "" + dataset_name + "/" + "wavs" + "/"
+  tf_s = "" + dataset_name + "/" + "wavs" + "/" + base
   target_path = Path(tp_s)
   target_file = Path(tf_s)
   print("From: " + str(filepath))
   print("To: " + str(target_file))
  
-  subprocess.run(["sox", "-G", "-v", "0.95", filepath, "48k.wav", "remix", "-", "rate", "48000"])
-  subprocess.run(["sox", "48k.wav", "-c", "1", "-r", "48000", "-b", "16", "-e", "signed-integer", "-t", "raw", "temp.raw"]) # convert wav to raw
-  subprocess.run([rnn, "temp.raw", "rnn.raw"]) # apply rnnoise
-  subprocess.run(["sox", "-G", "-v", "0.95", "-r", "48k", "-b", "16", "-e", "signed-integer", "rnn.raw", "-t", "wav", "rnn.wav"]) # convert raw back to wav
+  subprocess.run(["sox", "-G", "-v", "0.95", filepath, os.path.join(tempdir.name, "48k.wav"), "remix", "-", "rate", "48000"])
+  subprocess.run(["sox", os.path.join(tempdir.name, "48k.wav"), "-c", "1", "-r", "48000", "-b", "16", "-e", "signed-integer", "-t", "raw", os.path.join(tempdir.name, "temp.raw")]) # convert wav to raw
+  subprocess.run([rnn, os.path.join(tempdir.name,"temp.raw"), os.path.join(tempdir.name,"rnn.raw")]) # apply rnnoise
+  subprocess.run(["sox", "-G", "-v", "0.95", "-r", "48k", "-b", "16", "-e", "signed-integer", os.path.join(tempdir.name,"rnn.raw"), "-t", "wav", os.path.join(tempdir.name,"rnn.wav")]) # convert raw back to wav
  
   subprocess.run(["mkdir", "-p", str(target_path)])
-  subprocess.run(["sox", "rnn.wav", str(target_file), "remix", "-", "highpass", "100", "lowpass", "7000", "rate", "22050"]) # apply high/low pass filter and change sr to 22050Hz
+  subprocess.run(["sox", os.path.join(tempdir.name,"rnn.wav"), str(target_file), "remix", "-", "highpass", "100", "lowpass", "7000", "rate", "22050"]) # apply high/low pass filter and change sr to 22050Hz
   data, rate = sf.read(target_file)
  
 # peak normalize audio to -1 dB
@@ -41,3 +45,6 @@ for filepath in paths:
   loudness_normalized_audio = pyln.normalize.loudness(data, loudness, -25.0)
   sf.write(target_file, data=loudness_normalized_audio, samplerate=22050)
   print("")
+  
+  
+tempdir.cleanup()
