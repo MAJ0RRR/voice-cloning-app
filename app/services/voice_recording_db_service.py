baa36cb4656e61d31f44a787c89854665afb8a60
Dawid <dawid.rezmer@profil-software.com>
2023-02-25 06:44:27 +0100

    Add paging to choose_voice_model_view

--- app/services/voice_recording_db_service.py
+++ app/services/voice_recording_db_service.py
@@ -9,7 +9,7 @@ class VoiceRecordingDbService:
         self.conn = sqlite3.connect(db_file)
         self.cursor = self.conn.cursor()
         self.create_table()
-        if self.select_all_voice_records() == []:
+        if self.select_voice_recordings() == []:
             self.initialize()
 
     def create_table(self):
@@ -31,30 +31,60 @@ class VoiceRecordingDbService:
 
         return self.cursor.lastrowid
 
-    def select_model_id(self, model_id):
-        query = "SELECT v.id, v.name, v.path, v.model_id " \
-                "FROM voice_recordings AS v " \
-                "WHERE v.model_id = ?"
-        self.cursor.execute(query, (model_id,))
+
+    def select_voice_recordings(self, model_id=None, name=None):
+        if model_id:
+            if name:
+                query = "SELECT v.id, v.name, v.path, v.model_id " \
+                        "FROM voice_recordings AS v " \
+                        "WHERE v.model_id = ? and v.name = ?"
+                self.cursor.execute(query, (model_id, name))
+            else:
+                query = "SELECT v.id, v.name, v.path, v.model_id " \
+                        "FROM voice_recordings AS v " \
+                        "WHERE v.model_id = ?"
+                self.cursor.execute(query, (model_id,))
+        elif name:
+            query = "SELECT v.id, v.name, v.path, v.model_id " \
+                    "FROM voice_recordings AS v " \
+                    "WHERE v.name = ?"
+            self.cursor.execute(query, (name,))
+        else:
+            query = "SELECT v.id, v.name, v.path, v.model_id " \
+                        "FROM voice_recordings AS v "
+            self.cursor.execute(query)
         rows = self.cursor.fetchall()
         voice_records = []
         for row in rows:
-            voice_record = {"id": row[0], "name": row[1], "path": row[2], "model_id": row[3]}
-            voice_records.append(voice_record)
+             voice_record = {"id": row[0], "name": row[1], "path": row[2], "model_id": row[3]}
+             voice_records.append(voice_record)
         return voice_records
-
-    def select_all_voice_records(self):
-        query = "SELECT v.id, v.name, v.path " \
-                "FROM voice_recordings AS v"
-        self.cursor.execute(query)
-        rows = self.cursor.fetchall()
-        return rows
 
     def initialize(self):
         path1 = os.path.join(AUDIO_DIR, 'test1.mp3')
         path2 = os.path.join(AUDIO_DIR, 'test2.mp3')
+        path3 = os.path.join(AUDIO_DIR, 'test3.mp3')
+        path4 = os.path.join(AUDIO_DIR, 'test4.mp3')
+        path5 = os.path.join(AUDIO_DIR, 'test5.mp3')
+        path6 = os.path.join(AUDIO_DIR, 'test6.mp3')
+        path7 = os.path.join(AUDIO_DIR, 'test7.mp3')
+        path8 = os.path.join(AUDIO_DIR, 'test8.mp3')
+        path9 = os.path.join(AUDIO_DIR, 'test9.mp3')
+        path10 = os.path.join(AUDIO_DIR, 'test10.mp3')
+        path11 = os.path.join(AUDIO_DIR, 'test11.mp3')
+        path12 = os.path.join(AUDIO_DIR, 'test12.mp3')
         self.insert(VoiceRecording('basic', path1,1))
         self.insert(VoiceRecording('basic', path2,2))
+        self.insert(VoiceRecording('basic', path3, 3))
+        self.insert(VoiceRecording('basic', path4, 4))
+        self.insert(VoiceRecording('basic', path5, 5))
+        self.insert(VoiceRecording('basic', path6, 6))
+        self.insert(VoiceRecording('basic', path7, 7))
+        self.insert(VoiceRecording('basic', path8, 8))
+        self.insert(VoiceRecording('basic', path9, 9))
+        self.insert(VoiceRecording('basic', path10, 10))
+        self.insert(VoiceRecording('basic', path11,11))
+        self.insert(VoiceRecording('basic', path12, 12))
 
     def close(self):
         self.conn.close()
--- app/views/choose_voice_model_view.py
+++ app/views/choose_voice_model_view.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import playsound
 
 from lazy_import import lazy_module
 from app.components.listbox_with_button import ListBoxWithButton
@@ -9,32 +10,33 @@ main_menu_module = lazy_module("app.views.main_view")
 WIDTH = 1920
 BUTTON_WIDTH = 30
 BUTTON_HEIGHT = 3
-PAD_Y = 80
+PLAY_BUTTON_WIDTH = 10
+PLAY_BUTTON_HEIGHT = 1
+PAD_Y = 40
+MODELS_ON_PAGE = 10
 
 
 class ChooseVoiceModelView:
 
-    def __init__(self, root, gender, language, voice_model_service, voice_records_service):
+    def __init__(self, root, gender, language, voice_model_service, voice_recordings_service):
+        self.page = 0
         self.root = root
         self.voice_model_service = voice_model_service
-        self.voice_records_service = voice_records_service
-        models = self.voice_model_service.select_gender_language(gender, language)
+        self.voice_recordings_service = voice_recordings_service
+        self.model_entities = self.voice_model_service.select_gender_language(gender, language)
+        self.model_labels = []
+        self.model_buttons = []
+        self.display_models()
+        if self.has_next_page():
+            pass # create button
 
-        self.listbox = ListBoxWithButton(self.root)
-        for model in models:
-            audio_records = voice_records_service.select_model_id(model['id'])
-            for audio_record in audio_records:
-                if audio_record['name'] == 'basic':
-                    self.listbox.insert(tk.END, model['name'], audio_record['path'])
-                    break
-
-        b1 = tk.Button(self.root, text="Menu główne", command=self.switch_to_main_menu, width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
-        b2 = tk.Button(self.root, text="Cofnij",width=BUTTON_WIDTH, height=BUTTON_HEIGHT, command=self.switch_to_choose_gender_language_view)
-        b3 = tk.Button(self.root, text="Dalej",width=BUTTON_WIDTH, height=BUTTON_HEIGHT, command=self.switch_to_choose_audio_view)
-
-        b1.pack(pady=PAD_Y)
-        b2.pack(pady=PAD_Y)
-        b3.pack(pady=PAD_Y)
+        main_menu_button = tk.Button(self.root, text="Menu główne", command=self.switch_to_main_menu, width=BUTTON_WIDTH, height=BUTTON_HEIGHT)
+        back_button = tk.Button(self.root, text="Cofnij",width=BUTTON_WIDTH, height=BUTTON_HEIGHT, command=self.switch_to_choose_gender_language_view)
+        continue_button = tk.Button(self.root, text="Dalej",width=BUTTON_WIDTH, height=BUTTON_HEIGHT, command=self.switch_to_choose_audio_view)
+#
+        main_menu_button.place(x=200, y=700)
+        back_button.place(x=800, y=700)
+        continue_button.place(x=1400, y=700)
 
     def switch_to_choose_gender_language_view(self):
         for widget in self.root.winfo_children():
@@ -48,5 +50,47 @@ class ChooseVoiceModelView:
 
     def switch_to_choose_audio_view(self):
         pass
+
+    def display_models(self):
+       models =self.model_entities[self.page*10:self.page*10+10]
+       for model in models:
+           label = tk.Label(self.root, text=model["name"], bg='green', font=("Helvetica", 14))
+           label.place(x=WIDTH/2-200, y=150+len(self.model_labels*PAD_Y))
+           self.model_labels.append(label)
+           button = tk.Button(self.root, text="Odsłuchaj", width=PLAY_BUTTON_WIDTH, height=PLAY_BUTTON_HEIGHT,font=("Helvetica", 14), command=lambda: self.play_audio(model.id))
+           button.place(x=WIDTH/2+50, y=150 + len(self.model_buttons)*PAD_Y)
+           self.model_buttons.append(button)
+
+    def has_next_page(self):
+        return len(self.model_entities) > (self.page * 10 + 10)
+
+    def next_page(self):
+        pass
+        #delete all models labels and buttons
+        #increase self.page
+        #check if need to add previous page
+        #check if need to destroy next_page_button
+        #display models
+
+
+    def previous_page(self):
+        pass
+    # delete all models labels and buttons
+    # idecrease self.page
+    # check if need to add next page
+    # check if need to destroy previous_page_button
+    # display models
+
+
+    def play_audio(self, id):
+        if voice_recording := self.voice_recordings_service.select_voice_recordings(name='basic', model_id=id):
+            playsound(voice_recording['path'])
+        else:
+            print('error')
+
+
+
+
+
 
 
