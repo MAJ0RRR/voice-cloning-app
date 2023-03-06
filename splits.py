e932a66f19d939b1cf974de758dc594f127c9522
Kepins <sz.kepinski1@gmial.com>
2023-03-06 23:18:33 +0100

    Prototype of the algorithm

--- splits.py
+++ splits.py
@@ -1,8 +1,26 @@
 import os
 import glob
+import pydub
+import pathlib
 
 def split_file(file_path, out_dir):
-	os.system(f"sox {file_path} \"{out_dir}/$(basename -s .wav {file_path})\".wav --show-progress trim 0 8 : newfile : restart")
+	signal = pydub.AudioSegment.from_file(file_path, format='wav')
+	chunks = pydub.silence.split_on_silence(signal, min_silence_len=400, silence_thresh=-50, keep_silence=20)
+	
+	stem = pathlib.Path(file_path).stem
+	
+	# check if there are any chunks
+	#if not chunks:
+	#	file_name = f'CHUNK-0-{stem}.wav'
+	#	signal.export(os.path.join(out_dir, file_name), 'wav')
+	#	return
+	for i, chunk in enumerate(chunks):
+		print(len(chunk))
+		file_name = f'CHUNK-{i}-{stem}.wav'
+		chunk.export(os.path.join(out_dir, file_name), format='wav')
+	
+	#os.system(f"sox {file_path} {out_dir}/{stem}.wav --show-progress trim 0 8 : newfile : restart")
+	
 
 if __name__ == "__main__":
 	# clear temp directory
