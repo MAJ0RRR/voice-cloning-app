006df02cd56b9a0c4621e1ee7453a7b9eff8223c
Kepins <sz.kepinski1@gmial.com>
2023-03-08 18:56:22 +0100

    Choosing silence level and length to split

--- splits.py
+++ splits.py
@@ -2,43 +2,66 @@ import os
 import glob
 import pydub
 import pathlib
+import argparse
+import tempfile
 
-def split_file(file_path, out_dir):
-	signal = pydub.AudioSegment.from_file(file_path, format='wav')
-	chunks = pydub.silence.split_on_silence(signal, min_silence_len=400, silence_thresh=-50, keep_silence=20)
-	
-	stem = pathlib.Path(file_path).stem
-	
-	# check if there are any chunks
-	#if not chunks:
-	#	file_name = f'CHUNK-0-{stem}.wav'
-	#	signal.export(os.path.join(out_dir, file_name), 'wav')
-	#	return
-	for i, chunk in enumerate(chunks):
-		print(len(chunk))
-		file_name = f'CHUNK-{i}-{stem}.wav'
-		chunk.export(os.path.join(out_dir, file_name), format='wav')
-	
-	#os.system(f"sox {file_path} {out_dir}/{stem}.wav --show-progress trim 0 8 : newfile : restart")
+class FileSpliter:
+	def __init__(self, source, destination, silence_thresh, min_silence_len):
+		self.source = source
+		self.destination = destination
+		self.silence_thresh = silence_thresh
+		self.min_silence_len = min_silence_len
+		
+	def split(self):
+		audiofiles = glob.glob(self.source)
+		for audiofile in audiofiles:
+			self.__split_file(audiofile, self.destination)
+			
+	def __split_file(self, file_path, out_dir):
+		signal = pydub.AudioSegment.from_file(file_path, format='wav')
+		chunks = pydub.silence.split_on_silence(signal, min_silence_len=self.min_silence_len, silence_thresh=self.silence_thresh, keep_silence=50)
+		
+		stem = pathlib.Path(file_path).stem
+		
+		for i, chunk in enumerate(chunks):
+			file_name = f'CHUNK-{i}-{stem}.wav'
+			chunk.export(os.path.join(out_dir, file_name), format='wav')
+			
+		#os.system(f"sox {file_path} {out_dir}/{stem}.wav --show-progress trim 0 8 : newfile : restart")
 	
 
 if __name__ == "__main__":
-	# clear temp directory
-	os.system("rm audiofiles/temp/*")
+	parser = argparse.ArgumentParser(
+		prog = 'Splits',
+		description = 'Splits wav files into chunks on silence.'
+		)
+	parser.add_argument('-t', '--threshold', action='store', dest='silence_thresh', type=int,
+		default=-45, help='Threshold below which audio is considered silent. Default: -45.')
+	parser.add_argument('-l', '--length-silence', action='store', dest='min_silence_len', type=int,
+		default=300, help='Length of silence in miliseconds on which splitting happens. Default: 300.')
+	parser.add_argument('-s', '--source', action='store', dest='source', type=str,
+		default='audiofiles/raw', help='Path to raw audio. Default: audiofiles/raw')
+	parser.add_argument('-d', '--destiantion', action='store', dest='destination', type=str,
+		default='audiofiles/splits', help='Path to director to store audio chunks. Default: audiofiles/splits')
+	
+	parsed = parser.parse_args()
+	
+	# get tempdir
+	tempdir = tempfile.TemporaryDirectory()
 	
 	# clear splits directory
-	os.system("rm audiofiles/splits/*")
+	os.system(f"rm {parsed.destination}/*")
 	
 	# convert mp3 to proper wav
-	os.system("find audiofiles/raw -name '*.mp3' -exec bash -c 'for f; do ffmpeg -i \"$f\" -acodec pcm_s16le -ar 22050 -ac 1 \"audiofiles/temp/$(basename -s .mp3 $f)\".wav; done' _ {} +")
+	os.system(f"find {parsed.source} -name '*.mp3' -exec bash -c 'for f; do ffmpeg -i \"$f\" -acodec pcm_s16le -ar 22050 -ac 1 \"{tempdir.name}/$(basename -s .mp3 $f)\".wav -loglevel error; done' _ {{}} +")
 
 	# convert wav to proper wav
-	os.system("find audiofiles/raw -name '*.wav' -exec bash -c 'for f; do ffmpeg -y -i \"$f\" -acodec pcm_s16le -ar 22050 -ac 1 \"audiofiles/temp/$(basename -s .wav $f)\".wav; done' _ {} +")
-
-	# do split files
-	audiofiles = glob.glob("audiofiles/temp/*.wav")
-	for audiofile in audiofiles:
-		split_file(audiofile, "audiofiles/splits")
+	os.system(f"find {parsed.source} -name '*.wav' -exec bash -c 'for f; do ffmpeg -y -i \"$f\" -acodec pcm_s16le -ar 22050 -ac 1 \"{tempdir.name}/$(basename -s .wav $f)\".wav -loglevel error; done' _ {{}} +")
 	
-	# remove files that are to small
-	os.system("find audiofiles/splits -name \"*.wav\" -type f -size -30k -delete")
+	# do split files
+	fileSpliter = FileSpliter(source=f"{tempdir.name}/*.wav", destination=f"{parsed.destination}", silence_thresh=parsed.silence_thresh, min_silence_len=parsed.min_silence_len)
+	fileSpliter.split()
+	
+	# close tempdir
+	tempdir.cleanup()
+
