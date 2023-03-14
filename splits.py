51106b3b7e07c625103284ca67173e01f11479c3
Kepins <sz.kepinski1@gmial.com>
2023-03-14 19:37:57 +0100

    Files are not constantly loaded from files to RAM

--- splits.py
+++ splits.py
@@ -17,54 +17,28 @@ class FileSpliter:
 		self.max_audio_len = 15000
 		
 	def split(self):
-		# get tempdir
-		tempdir = tempfile.TemporaryDirectory()
-		
-		chunk_nums_temp = {}
-		chunk_nums_out = {}
-
-		# move all files to tempdir
+		# iterate over all files
 		audiofiles_src = glob.glob(self.source)
 		for audiofile in audiofiles_src:
 			stem = pathlib.Path(audiofile).stem
-			dir_per_audio = os.path.join(tempdir.name, stem)
-			os.mkdir(dir_per_audio)
-			chunk_nums_temp[stem] = 1
-			chunk_nums_out[stem] = 0
-			shutil.move(audiofile, os.path.join(dir_per_audio, '0.wav'))
-		
-		for silence_thresh, min_silence_len in zip(self.silence_threshs, self.min_silence_lens):
-			stems = chunk_nums_temp.keys()
-
-			for stem in stems:
-				print(stem)
-
-				dir = os.path.join(tempdir.name, stem)
-				audiofiles = glob.glob(dir + '/*.wav')
-				
-				for audiofile in audiofiles:
-					signal = pydub.AudioSegment.from_file(audiofile, format='wav')
-					chunks = pydub.silence.split_on_silence(signal, min_silence_len=min_silence_len, silence_thresh=silence_thresh, keep_silence=50)
-					
-					for chunk in chunks:
-						if len(chunk) < self.min_audio_len:
+			chunk_num_out = 0
+			signal = pydub.AudioSegment.from_file(audiofile, format='wav')
+			chunks = [signal]
+			for silence_thresh, min_silence_len in zip(self.silence_threshs, self.min_silence_lens):
+				chunks_new_iter = []
+				for chunk in chunks:
+					new_chunks = pydub.silence.split_on_silence(chunk, min_silence_len=min_silence_len, silence_thresh=silence_thresh, keep_silence=50)
+					for new_chunk in new_chunks:
+						if len(new_chunk) < self.min_audio_len:
 							pass
-						elif len(chunk) > self.max_audio_len:
-							chunk_num_temp = chunk_nums_temp[stem]
-							chunk.export(os.path.join(dir, f'{chunk_num_temp}.wav'), format='wav')
-							chunk_nums_temp[stem] = chunk_num_temp + 1
+						elif len(new_chunk) > self.max_audio_len:
+							chunks_new_iter.append(new_chunk)
 						else:
-							chunk_num_out = chunk_nums_out[stem]
-							chunk.export(os.path.join(self.destination, f'{stem}-CHUNK-{chunk_num_out}.wav'), format='wav')
-							chunk_nums_out[stem] = chunk_num_out + 1
-					# delete file from tempdir
-					os.remove(audiofile)
+							new_chunk.export(os.path.join(self.destination, f'{stem}-CHUNK-{chunk_num_out}.wav'), format='wav')
+							chunk_num_out = chunk_num_out + 1
+				chunks = chunks_new_iter	
 
-					# os.system(f"sox {file_path} {out_dir}/{stem}.wav --show-progress trim 0 8 : newfile : restart")
-
-		# close tempdir
-		tempdir.cleanup()
-		
+		# os.system(f"sox {file_path} {out_dir}/{stem}.wav --show-progress trim 0 8 : newfile : restart")
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(
@@ -72,9 +46,9 @@ if __name__ == "__main__":
 		description = 'Splits wav files into chunks on silence.'
 		)
 	parser.add_argument('-t', '--thresholds', action='store', dest='silence_threshs', nargs="*", type=int,
-		default=[-45], help='Threshold below which audio is considered silent. Default: [-45].')
+		default=[-45], help='Thresholds below which audio is considered silent. Default: [-45].')
 	parser.add_argument('-l', '--lengths-silence', action='store', dest='min_silence_lens', nargs="*", type=int,
-		default=[300], help='Length of silence in miliseconds on which splitting happens. Default: [300].')
+		default=[300], help='Lengths of silence in miliseconds on which splitting happens. Default: [300].')
 	parser.add_argument('-s', '--source', action='store', dest='source', type=str,
 		default='audiofiles/raw', help='Path to raw audio. Default: audiofiles/raw')
 	parser.add_argument('-d', '--destiantion', action='store', dest='destination', type=str,
