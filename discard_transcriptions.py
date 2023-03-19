6e67236093aca67c3775885b6466e394901a8e05
MAJ0RRR <sebastian.lesniewski.edu@gmail.com>
2023-03-19 20:37:27 +0100

    Add english alphabet and update file paths

--- discard_transcriptions.py
+++ discard_transcriptions.py
@@ -1,25 +1,66 @@
+"""
+This script discards transcriptions that contain illegal symbols from `metadata.csv` file and deltes according `*.wav` file.
+
+Usage: # Order of arguments matter!
+------
+1) Default language and default location
+`python3 ./discard.transcriptions.py` 
+------
+2) Custom language and default location
+`python3 ./discard.transcriptions.py pl`
+`python3 ./discard.transcriptions.py en`
+------
+3) Custom language and custom location
+`python3 ./discard.transcriptions.py pl /PATH/TO/DATASET`
+`python3 ./discard.transcriptions.py en /PATH/TO/DATASET`
+
+#TODO:
+- Discard very short transcriptions
+------
+"""
+
 import csv
 import os
+import sys
 
 def is_valid_string(string,alphabet,symbols):
     for char in string.upper():
         if char not in alphabet:
-            return char in symbols
+            if char not in symbols:
+                return False
     return True
 
 if __name__ == "__main__":
-    polish_alphabet = ['A', 'Ą', 'B', 'C', 'Ć', 'D', 'E', 'Ę', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'Ł', 'M', 'N', 'Ń', 'O', 'Ó', 'P', 'Q', 'R', 'S', 'Ś', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'Ź', 'Ż']
-    symbols = [' ',',','.','?','!']
+    polish_alphabet = list("AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻ")
+    english_alphabet = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    symbols = list(" ,.?!")
+
+    alphabets = {
+        "pl" : polish_alphabet,
+        "en" : english_alphabet
+    }
+
+    alphabet = []
+    root = 'audiofiles/datasets/dataset'
+
+    if len(sys.argv) == 1: # Default language in default lcoation
+        alphabet = polish_alphabet
+    elif len(sys.argv) == 2: # Custom language in default location
+        alphabet = alphabets[str(sys.argv[1])]
+    else: # Custom language in custom location
+        alphabet = alphabets[str(sys.argv[1])]
+        root = str(sys.argv[2])
     
-    with open('audiofiles/datasets/dataset/metadata.csv') as orginal, \
-        open('audiofiles/datasets/dataset/filtered.csv', 'w') as filtered:
+    with open(os.path.join(root,'metadata.csv')) as orginal, \
+        open(os.path.join(root,'filtered.csv'), 'w') as filtered:
         transcript = csv.reader(orginal, delimiter='|')
 
         for row in transcript:
             valid = True
             for substring in str.split(row[1]," "):
-                if not is_valid_string(substring,polish_alphabet,symbols):
+                if not is_valid_string(substring,alphabet,symbols):
                     valid = False
+                    break
 
             if valid:
                 for element in row[:-1]:
@@ -29,11 +70,11 @@ if __name__ == "__main__":
                 filtered.write("\n")
 
             else:
-                os.remove(f"audiofiles/wavs/{row[0]}.wav")
+                if os.path.exists(os.path.join(root,f"wavs/{row[0]}.wav")):
+                    print(f"Removing: {row[0]}.wav\n")
+                    os.remove(os.path.join(root,f"wavs/{row[0]}.wav"))
+                else:
+                    print(f"Tried to remove {row[0]}.wav, but it does not exist.")
 
-    os.remove('audiofiles/datasets/dataset/metadata.csv')
-    os.rename('audiofiles/datasets/dataset/filtered.csv','audiofiles/datasets/dataset/metadata.csv')
-
-
-
-
+    os.remove(os.path.join(root,'metadata.csv'))
+    os.rename(os.path.join(root,'filtered.csv'),os.path.join(root,'metadata.csv'))
