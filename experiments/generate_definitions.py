a02d89ace714f77a3fe8cdaa2b5cbb3badcec76d
Piotr Sieński <s184297@student.pg.edu.pl>
2023-03-29 22:44:33 +0200

    minor fixes

--- experiments/generate_definitions.py
+++ experiments/generate_definitions.py
@@ -1,7 +1,6 @@
 import itertools
 from typing import List
 
-from whole_pipeline import run_pipeline
 import json
 
 DEFINITIONS_FILE_NAME = "definitions.json"
@@ -11,7 +10,13 @@ def _set_definition_values(definition: dict, name: str, source: dict, trim_len: 
                            split_type: str, remove_noise: bool, discard: int):
     definition["Name"] = name
     definition["RawSource"] = source["RawSource"]
-    definition["TrimSourceLengthMs"] = trim_len * 60 * 1000
+
+    if trim_len == 0:
+        definition["TrimSource"] = False
+    else:
+        definition["TrimSource"] = False
+        definition["TrimSourceLengthMs"] = trim_len * 60 * 1000
+
     definition["ModelPath"] = source["ModelPath"]
     definition["Language"] = source["Language"]
     definition["SilenceSplitType"] = split_type
@@ -27,7 +32,8 @@ def generate_definitions(name_idx_start: int = 0, sources: List[dict] = None,
                          trim_source_lengths_mins: List[int] = None,
                          silence_split_types: List[str] = None, split_lengths: List[int] = None,
                          split_min_silence_lens: List[int] = None, split_silence_threshs: List[int] = None,
-                         remove_noises: List[bool] = None, discard_word_counts: List[int] = None):
+                         remove_noises: List[bool] = None, discard_word_counts: List[int] = None,
+                         whisper_vram: int = 10, gpu: int = 2):
     """
     :param name_idx_start: Start index for experiment names
     :param sources: List of dicts - elements should be given in format
@@ -38,8 +44,7 @@ def generate_definitions(name_idx_start: int = 0, sources: List[dict] = None,
     :param split_min_silence_lens: List of min silence lengths for split_silence
     :param split_silence_threshs: List of silence threshs for split_silence
     :param remove_noises: List - elements can be either true or false
-    :param discard_transcripts: List - elements can be either true or false
-    :param discard_word_counts: List - elements can be either true or false
+    :param discard_word_counts: List of ints - 0 means no transcripts are discarded
     :return: None
     """
     assert len(split_min_silence_lens) == len(split_silence_threshs)
@@ -56,6 +61,8 @@ def generate_definitions(name_idx_start: int = 0, sources: List[dict] = None,
                                        trim_len, split_type, remove_noise, discard)
                 definition["SplitSilenceLength"] = length
                 defs.append(definition)
+                name_idx_count += 1
+
         elif split_type == 'silence':
             for silence_len, thresh in zip(split_min_silence_lens, split_silence_threshs):
                 definition = {}
@@ -64,9 +71,16 @@ def generate_definitions(name_idx_start: int = 0, sources: List[dict] = None,
                 definition["SplitSilenceMinLength"] = silence_len
                 definition["SplitSilenceThresh"] = thresh
                 defs.append(definition)
+                name_idx_count += 1
+
+    output = {
+        "Definitions": defs,
+        "Gpu": gpu,
+        "WhisperVram": whisper_vram
+    }
 
     with open(DEFINITIONS_FILE_NAME, 'w') as file:
-        file.write(json.dumps(defs))
+        file.write(json.dumps(output))
 
 
 if __name__ == '__main__':
