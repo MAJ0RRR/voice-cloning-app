5db4e61c1a7d9f23255299daa4f9ef2f80970dd3
Dawid <dawid.rezmer@profil-software.com>
2023-03-30 13:37:07 +0200

    Fix initialize.py

--- initialize.py
+++ initialize.py
@@ -1,165 +1,97 @@
-from trainer import Trainer, TrainerArgs
-from TTS.config import load_config
-from TTS.tts.configs.shared_configs import BaseDatasetConfig
-from TTS.tts.configs.vits_config import VitsConfig
-from TTS.tts.datasets import load_tts_samples
-from TTS.tts.models.vits import Vits, VitsAudioConfig
-from TTS.tts.utils.text.tokenizer import TTSTokenizer
-from TTS.utils.audio import AudioProcessor
 import os
-import argparse
-import json
-
-OUTPUT_PATH = "output/"
-CONFIG_FILE_NAME = "config.json"
-DEFAULT_RUN_NAME = "new_model"
-DATASETS_DIR = "audiofiles/datasets"
-TEST_SENTENCES_FILE = "test_sentences.json"
+import sys
+import shutil
+import subprocess
 
 
-def validate_input(args):
-    assert int(args.gpu_num) >= 0, f'gpu_num is smaller than 0, gpu_num={args.gpu_num}'
+def create_venv(project_root):
+    # path to new venv
+    venv_path = os.path.join(project_root, 'venv')
 
-    assert args.language in ('en', 'pl'), f'incorrect language ID, language={args.language}'
-
-    assert args.run_name, f'run_name cannot be empty'
-
-    assert args.dataset_name, f'dataset_name cannot be empty'
-    assert os.path.exists(os.path.join(DATASETS_DIR, args.dataset_name)), \
-        f'directory dataset_name does not exist, dataset_name={args.dataset_name}'
-
-    if args.model_path:
-        assert args.model_path.endswith('.pth'), f'model_path file type incorrect, model_path={args.model_path}'
-
-        assert os.path.exists(os.path.join(OUTPUT_PATH, args.model_path)),\
-            f'model_path file type incorrect, model_path={args.model_path}'
-
-        model_dir_path = os.path.join(*args.model_path.split('/')[0:-1])
-        assert CONFIG_FILE_NAME in os.listdir(os.path.join(OUTPUT_PATH, model_dir_path)),\
-            f'model_path directory does not contain {CONFIG_FILE_NAME} file file'
+    # create venv
+    return os.system('python3.9 -m venv ' + venv_path + ' 2> /dev/null')
 
 
-def train(model_path, dataset_name, language, run_name):
-    mode = 'continue' if model_path else 'new'
+def install_requirements(project_root):
+    # path to exisitng venv
+    venv_path = os.path.join(project_root, 'venv')
 
-    dataset_config = BaseDatasetConfig(
-        formatter="ljspeech", meta_file_train="metadata.csv", path=os.path.join(DATASETS_DIR, dataset_name))
+    # path to requirements.txt
+    req_path = os.path.join(project_root, 'requirements.txt')
 
-    audio_config = VitsAudioConfig(
-        sample_rate=22050, win_length=1024, hop_length=256, num_mels=80, mel_fmin=0, mel_fmax=None
-    )
-
-    print(f"Mode=={mode}\n")
-
-    if mode == 'new':
-        config = VitsConfig(
-            audio=audio_config,
-            run_name=run_name,
-            batch_size=20,
-            eval_batch_size=20,
-            batch_group_size=4,
-            # num_loader_workers=8,
-            num_loader_workers=4,
-            num_eval_loader_workers=4,
-            run_eval=True,
-            test_delay_epochs=-1,
-            epochs=100000,
-            save_step=10000,
-            save_checkpoints=True,
-            save_n_checkpoints=10,
-            save_best_after=1000,
-            # text_cleaner="english_cleaners",
-            text_cleaner="multilingual_cleaners",
-            eval_split_size=0.1,
-            use_phonemes=True,
-            phoneme_language=language,
-            phoneme_cache_path=os.path.join(OUTPUT_PATH, "phoneme_cache"),
-            compute_input_seq_cache=True,
-            print_step=10,
-            print_eval=True,
-            mixed_precision=True,
-            output_path=OUTPUT_PATH,
-            datasets=[dataset_config],
-            cudnn_benchmark=False,
-        )
-    else:
-        model_dir = model_path[:model_path.rfind("/")]
-        config = load_config(os.path.join(OUTPUT_PATH, model_dir, CONFIG_FILE_NAME))
-        config.run_name = run_name
-        config.output_path = os.path.join(os.getcwd(), OUTPUT_PATH)
-        config.phoneme_cache_path = os.path.join(os.getcwd(), OUTPUT_PATH, 'phoneme_cache')
-        config.datasets[0]["path"] = os.path.join(os.getcwd(), DATASETS_DIR, dataset_name)
-        config.restore_path = model_path
-
-    try:
-     with open(TEST_SENTENCES_FILE, "r", encoding="utf-8") as json_file:
-        test_sentences = json.load(json_file)
-        config.test_sentences = test_sentences[language]
-    except FileNotFoundError:
-        print(f"Error: JSON file {TEST_SENTENCES_FILE} not found.")
-    except json.JSONDecodeError:
-        print(f"Error: Invalid JSON format in file {TEST_SENTENCES_FILE}.")
-    except Exception as e:
-        print(f"Error: {e}")
+    # install requirements
+    return os.system(os.path.join(venv_path, 'bin/pip3.9') + ' install -r ' + req_path)
 
 
-    #print(config)
-    # INITIALIZE THE AUDIO PROCESSOR
-    # Audio processor is used for feature extraction and audio I/O.
-    # It mainly serves to the dataloader and the training loggers.
-    ap = AudioProcessor.init_from_config(config)
+def initialize_directories(project_root):
+    # Create all necessary directories
 
-    # INITIALIZE THE TOKENIZER
-    # Tokenizer is used to convert text to sequences of token IDs.
-    # config is updated with the default characters if not defined in the config.
-    tokenizer, config = TTSTokenizer.init_from_config(config)
+    necessary_directories = ['audiofiles', 'audiofiles/raw', 'audiofiles/splits', 'audiofiles/datasets',
+                             'output',
+                             'tools',
+                             ]
 
-    # LOAD DATA SAMPLES
-    # Each sample is a list of [text, audio_file_path, speaker_name]
-    # You can define your custom sample loader returning the list of samples.
-    # Or define your custom formatter and pass it to the load_tts_samples.
-    # Check TTS.tts.datasets.load_tts_samples for more details.
-    train_samples, eval_samples = load_tts_samples(
-        dataset_config,
-        eval_split=True,
-        eval_split_max_size=config.eval_split_max_size,
-        eval_split_size=config.eval_split_size,
-    )
-
-    model = Vits(config, ap, tokenizer, speaker_manager=None)
-    if mode == 'continue':
-        model.load_checkpoint(config, os.path.join(OUTPUT_PATH, model_path))
-
-    # init the trainer and begin
-    trainer = Trainer(
-        TrainerArgs(),
-        config,
-        OUTPUT_PATH,
-        model=model,
-        train_samples=train_samples,
-        eval_samples=eval_samples,
-    )
-    trainer.fit()
+    for nd in necessary_directories:
+        path = os.path.join(project_root, nd)
+        if not os.path.exists(path):
+            os.mkdir(path)
 
 
-if __name__=='__main__':
+def install_rnnoise(project_root):
+    rnnoise_path = os.path.join(project_root, 'tools/rnnoise')
 
-    parser = argparse.ArgumentParser(prog='Train',
-                                     description='Trains model from scratch or continues training of given model')
-    parser.add_argument('-m', '--model_path', action='store', dest='model_path', default=None,
-                        help='Model path (from output/), starts from scratch if not given.'
-                             ' There must be config.json next to model!')
-    parser.add_argument('-n', '--run_name', action='store', dest='run_name', default=DEFAULT_RUN_NAME,
-                        help=f'Run name. Default {DEFAULT_RUN_NAME}')
-    parser.add_argument('-l', '--language', action='store', dest='language', default='en',
-                        help='Language of model (en/pl). Default: en.')
-    parser.add_argument('-d', '--dataset', action='store', dest='dataset_name', default='dataset',
-                        help=f'Dataset name (from {DATASETS_DIR}). Default: dataset.')
-    parser.add_argument('-g', '--gpu', action='store', dest='gpu_num', required=True, help='GPU number.')
+    if os.path.exists(rnnoise_path):
+        shutil.rmtree(rnnoise_path)
 
-    args = parser.parse_args()
-    validate_input(args)
+    commands = [(os.system, 'git clone https://github.com/xiph/rnnoise.git ' + rnnoise_path),
+                (os.chdir, rnnoise_path),
+                (os.system, 'sh autogen.sh'),
+                (os.system, 'sh configure'),
+                (os.system, 'make clean'),
+                (os.system, 'make rnnoise'),
+                (os.chdir, project_root)
+                ]
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = args.gpu_num
+    for command, arg in commands:
+        return_value = command(arg)
+        if return_value is not None and return_value != 0:
+            os.chdir(project_root)
+            return return_value, arg
 
-    train(args.model_path, args.dataset_name, args.language, args.run_name)
+    return 0, None
+
+
+def initialize():
+    # set this to project root
+    project_root = os.getcwd()
+
+    # create venv with requirements
+    if create_venv(project_root) != 0:
+        print('\nPython 3.9 with venv is not installed!')
+        sys.exit()
+
+    # install requirements to venv
+    if install_requirements(project_root) != 0:
+        print('\nErorr while installing requirements')
+        sys.exit()
+
+    # create all directories
+    initialize_directories(project_root)
+
+    ret_val, failed_command = install_rnnoise(project_root)
+    # install rnnoise
+    if ret_val != 0:
+        print('\nErorr while installing rnnoise')
+        sys.exit()
+
+    # print on success
+    print('\nInitialized successfully')
+
+
+def download_models():
+    os.system('venv/bin/python download_models.py')
+
+
+if __name__ == '__main__':
+    initialize()
+    download_models()
