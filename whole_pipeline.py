706d9b0af80731405fcac69eb0794c59e13cb575
Piotr Sieński <sienskipiotr2@gmail.com>
2023-05-10 20:31:57 +0000

    added timeout for running experiments

--- whole_pipeline.py
+++ whole_pipeline.py
@@ -1,5 +1,6 @@
 import os
-import wave
+import subprocess
+import time
 from pydub import AudioSegment
 from distutils.dir_util import copy_tree
 import glob
@@ -11,7 +12,30 @@ from whispertrans import create_transcription
 from discard_transcriptions import discard_transcriptions
 from train import train
 
+def run_and_kill_after(func, timeout):
+    p = subprocess.Popen(["python", "-c", func], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    time_count = 0
+    step = 10
+    while time_count < timeout:
+        print(f'Function is already running for {time_count} / {timeout} seconds, waiting for additional {step} seconds')
+        time_count += step
+        time.sleep(step)
+        
+        return_code = p.poll()
+        if return_code is not None:
+            print('Got output from function:')
+            stdout, stderr = p.communicate()
+            print('STDOUT:')
+            print(stdout.decode())
+            print('STDERR:')
+            print(stderr.decode())
+            print(f'Process finished with code: {return_code}')
+            break
 
+    if p.poll() is None:
+        p.kill()
+        raise TimeoutError("Subprocess timed out after {} seconds".format(timeout))
+    
 def trim_wavs(source, destinantion, desired_len_ms):
     len_sum = 0
     audiofiles_src = glob.glob(source + "/*")
@@ -39,7 +63,7 @@ def trim_wavs(source, destinantion, desired_len_ms):
 def run_pipeline(gpu_num, experiment_dir, raw_source, trim_source_length=0, silence_split_type="equal", split_len=8,
                  split_min_silence_lens=None, split_silence_threshs=None, remove_noises=True, dataset_name="dataset",
                  whisper_vram=10, discard_transcripts=True, discard_word_count=3, language="en",
-                 model_path=None, run_name="experiment"):
+                 model_path=None, run_name="experiment", timeout_seconds=1200):
     if split_silence_threshs is None:
         split_silence_threshs = [-45]
     if split_min_silence_lens is None:
@@ -48,6 +72,9 @@ def run_pipeline(gpu_num, experiment_dir, raw_source, trim_source_length=0, sile
     splits_dir = os.path.join(experiment_dir, 'splits')
     datasets_dir = os.path.join(experiment_dir, 'datasets')
     dataset_dir = os.path.join(datasets_dir, dataset_name)
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_num)
+    ''' 
     if not os.path.exists(splits_dir):
         os.makedirs(splits_dir)
     if not os.path.exists(dataset_dir):
@@ -59,7 +86,6 @@ def run_pipeline(gpu_num, experiment_dir, raw_source, trim_source_length=0, sile
         audio_source = os.path.join(experiment_dir, 'trimmed')
         trim_wavs(raw_source, audio_source, trim_source_length)
 
-    os.environ["CUDA_VISIBLE_DEVICES"] = str(gpu_num)
     if silence_split_type == "equal":
         split_equal(splits_dir, audio_source, length=split_len)
     else:
@@ -74,7 +100,12 @@ def run_pipeline(gpu_num, experiment_dir, raw_source, trim_source_length=0, sile
 
     if discard_transcripts:
         discard_transcriptions(language, dataset_dir, discard_word_count)
-
-        
-    train("", model_path, dataset_name, language, run_name, datasets_dir)
+   '''
+    train_func = """from train import train\ntrain( "", '{}', '{}', '{}', '{}', '{}')""".format(
+        model_path, dataset_name,language, run_name, datasets_dir)
+    print(train_func)
+    try:
+        run_and_kill_after(train_func, timeout_seconds)
+    except TimeoutError as e:
+        print(str(e))
 
